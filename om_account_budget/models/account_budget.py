46b727c355f5c598d62f9f3c38ac4022ee2e41f7
Mohamed Ba <mohamed.abdoulah.ba@gmail.com>
2024-10-28 14:08:24 +0000

    FIX practical compute amount

--- om_account_budget/models/account_budget.py
+++ om_account_budget/models/account_budget.py
@@ -161,39 +161,39 @@ class CrossoveredBudgetLines(models.Model):
                 computed_name += ' - ' + line.analytic_account_id.name
             line.name = computed_name
 
+
     def _compute_practical_amount(self):
-        for line in self:
-            acc_ids = line.general_budget_id.account_ids.ids
-            date_to = line.date_to
-            date_from = line.date_from
-            if line.analytic_account_id.id:
-                analytic_line_obj = self.env['account.analytic.line']
-                domain = [('account_id', '=', line.analytic_account_id.id),
-                          ('date', '>=', date_from),
-                          ('date', '<=', date_to),
-                          ]
-                if acc_ids:
-                    domain += [('general_account_id', 'in', acc_ids)]
+            for line in self:
+                acc_ids = line.general_budget_id.account_ids.ids
+                date_to = line.date_to
+                date_from = line.date_from
+                if line.analytic_account_id.id:
+                    analytic_line_obj = self.env['account.analytic.line']
+                    domain = [
+                        ('account_id', '=', line.analytic_account_id.id),
+                        ('date', '>=', date_from),
+                        ('date', '<=', date_to),
+                              ]
+                    if acc_ids:
+                        domain += [('general_account_id', 'in', acc_ids)]
 
-                where_query = analytic_line_obj._where_calc(domain)
-                analytic_line_obj._apply_ir_rules(where_query, 'read')
-                from_clause, where_clause, where_clause_params = where_query.get_sql()
-                select = "SELECT SUM(amount) from " + from_clause + " where " + where_clause
-
-            else:
+                    where_query = analytic_line_obj._where_calc(domain)
+                    analytic_line_obj._apply_ir_rules(where_query, 'read')
+                    from_clause, where_clause, where_clause_params = where_query.get_sql()
+                    select = "SELECT SUM(amount) from " + from_clause + " where " + where_clause
                 aml_obj = self.env['account.move.line']
-                domain = [('account_id', 'in',
-                           line.general_budget_id.account_ids.ids),
-                          ('date', '>=', date_from),
-                          ('date', '<=', date_to)
-                          ]
+                domain = [('account_id', 'in', line.general_budget_id.account_ids.ids),
+                            ('date', '>=', date_from),
+                            ('date', '<=', date_to),
+                            
+                            ]
                 where_query = aml_obj._where_calc(domain)
                 aml_obj._apply_ir_rules(where_query, 'read')
                 from_clause, where_clause, where_clause_params = where_query.get_sql()
                 select = "SELECT sum(credit)-sum(debit) from " + from_clause + " where " + where_clause
-
-            self.env.cr.execute(select, where_clause_params)
-            line.practical_amount = self.env.cr.fetchone()[0] or 0.0
+            
+                self.env.cr.execute(select, where_clause_params)
+                line.practical_amount = self.env.cr.fetchone()[0] or 0.0
 
     def _compute_theoritical_amount(self):
         # beware: 'today' variable is mocked in the python tests and thus, its implementation matter
